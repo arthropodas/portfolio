890f32bacb539a4c0920dafc067895d5789a9213
arthropodas <asifptb456@gmail.com>
2024-07-11 20:07:08 +0530

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Output directory for the build
+    outDir: 'dist'
   },
-  base: '/portfolio/' // Base URL for GitHub Pages deployment
+  base: '' 
 });
